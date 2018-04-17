bb649bb0fc6ddff607dc220c99bbe62dc2d5f88d
Daniil Digtyar <daniildigtyar@gmail.com>
2018-04-17 16:00:13 +0200

    New version of login

--- AM3D/Form1.Designer.vb
+++ AM3D/Form1.Designer.vb
@@ -34,17 +34,17 @@ Partial Class Form1
         Me.CheckBoxRecordar = New System.Windows.Forms.CheckBox()
         Me.ButtonLogin = New System.Windows.Forms.Button()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
         Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Panel1.SuspendLayout()
         Me.Panel2.SuspendLayout()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
@@ -52,7 +52,7 @@ Partial Class Form1
         Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
         Me.PictureBox1.Location = New System.Drawing.Point(6, 6)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(50, 62)
+        Me.PictureBox1.Size = New System.Drawing.Size(32, 40)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
@@ -60,10 +60,10 @@ Partial Class Form1
         'LabelLogo
         '
         Me.LabelLogo.AutoSize = True
-        Me.LabelLogo.Font = New System.Drawing.Font("Copperplate Gothic Light", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelLogo.Location = New System.Drawing.Point(62, 33)
+        Me.LabelLogo.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelLogo.Location = New System.Drawing.Point(44, 15)
         Me.LabelLogo.Name = "LabelLogo"
-        Me.LabelLogo.Size = New System.Drawing.Size(263, 35)
+        Me.LabelLogo.Size = New System.Drawing.Size(158, 25)
         Me.LabelLogo.TabIndex = 1
         Me.LabelLogo.Text = "AMD3D - Login"
         '
@@ -91,22 +91,22 @@ Partial Class Form1
         'LabelUsuari
         '
         Me.LabelUsuari.AutoSize = True
-        Me.LabelUsuari.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelUsuari.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LabelUsuari.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.LabelUsuari.Location = New System.Drawing.Point(109, 38)
+        Me.LabelUsuari.Location = New System.Drawing.Point(52, 32)
         Me.LabelUsuari.Name = "LabelUsuari"
-        Me.LabelUsuari.Size = New System.Drawing.Size(62, 22)
+        Me.LabelUsuari.Size = New System.Drawing.Size(51, 18)
         Me.LabelUsuari.TabIndex = 5
         Me.LabelUsuari.Text = "Usuari"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(109, 122)
+        Me.Label1.Location = New System.Drawing.Point(52, 115)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(112, 22)
+        Me.Label1.Size = New System.Drawing.Size(92, 18)
         Me.Label1.TabIndex = 6
         Me.Label1.Text = "Contrasenya"
         '
@@ -114,23 +114,23 @@ Partial Class Form1
         '
         Me.TextBoxUsuari.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.TextBoxUsuari.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.TextBoxUsuari.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxUsuari.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBoxUsuari.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.TextBoxUsuari.Location = New System.Drawing.Point(63, 72)
+        Me.TextBoxUsuari.Location = New System.Drawing.Point(6, 56)
         Me.TextBoxUsuari.Name = "TextBoxUsuari"
-        Me.TextBoxUsuari.Size = New System.Drawing.Size(379, 30)
+        Me.TextBoxUsuari.Size = New System.Drawing.Size(379, 24)
         Me.TextBoxUsuari.TabIndex = 7
         '
         'TextBoxContrasenya
         '
         Me.TextBoxContrasenya.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
         Me.TextBoxContrasenya.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.TextBoxContrasenya.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxContrasenya.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBoxContrasenya.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.TextBoxContrasenya.Location = New System.Drawing.Point(64, 155)
+        Me.TextBoxContrasenya.Location = New System.Drawing.Point(7, 139)
         Me.TextBoxContrasenya.Name = "TextBoxContrasenya"
         Me.TextBoxContrasenya.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
-        Me.TextBoxContrasenya.Size = New System.Drawing.Size(377, 30)
+        Me.TextBoxContrasenya.Size = New System.Drawing.Size(377, 24)
         Me.TextBoxContrasenya.TabIndex = 8
         '
         'CheckBoxRecordar
@@ -138,11 +138,11 @@ Partial Class Form1
         Me.CheckBoxRecordar.AutoSize = True
         Me.CheckBoxRecordar.Cursor = System.Windows.Forms.Cursors.Default
         Me.CheckBoxRecordar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.CheckBoxRecordar.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBoxRecordar.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.CheckBoxRecordar.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.CheckBoxRecordar.Location = New System.Drawing.Point(242, 197)
+        Me.CheckBoxRecordar.Location = New System.Drawing.Point(189, 169)
         Me.CheckBoxRecordar.Name = "CheckBoxRecordar"
-        Me.CheckBoxRecordar.Size = New System.Drawing.Size(157, 26)
+        Me.CheckBoxRecordar.Size = New System.Drawing.Size(133, 22)
         Me.CheckBoxRecordar.TabIndex = 9
         Me.CheckBoxRecordar.Text = "Recordar usuari"
         Me.CheckBoxRecordar.UseVisualStyleBackColor = True
@@ -151,9 +151,9 @@ Partial Class Form1
         '
         Me.ButtonLogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(114, Byte), Integer), CType(CType(127, Byte), Integer))
         Me.ButtonLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.ButtonLogin.Font = New System.Drawing.Font("Maiandra GD", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonLogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ButtonLogin.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.ButtonLogin.Location = New System.Drawing.Point(63, 253)
+        Me.ButtonLogin.Location = New System.Drawing.Point(6, 237)
         Me.ButtonLogin.Name = "ButtonLogin"
         Me.ButtonLogin.Size = New System.Drawing.Size(378, 40)
         Me.ButtonLogin.TabIndex = 10
@@ -170,24 +170,8 @@ Partial Class Form1
         Me.Panel1.Controls.Add(Me.PictureBox4)
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(500, 80)
+        Me.Panel1.Size = New System.Drawing.Size(500, 50)
         Me.Panel1.TabIndex = 11
-        '
-        'Panel2
-        '
-        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.Panel2.Controls.Add(Me.PictureBox5)
-        Me.Panel2.Controls.Add(Me.PictureBox3)
-        Me.Panel2.Controls.Add(Me.TextBoxContrasenya)
-        Me.Panel2.Controls.Add(Me.LabelUsuari)
-        Me.Panel2.Controls.Add(Me.ButtonLogin)
-        Me.Panel2.Controls.Add(Me.Label1)
-        Me.Panel2.Controls.Add(Me.CheckBoxRecordar)
-        Me.Panel2.Controls.Add(Me.TextBoxUsuari)
-        Me.Panel2.Location = New System.Drawing.Point(0, 71)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(500, 328)
-        Me.Panel2.TabIndex = 12
         '
         'ComboBox1
         '
@@ -201,33 +185,49 @@ Partial Class Form1
         Me.ComboBox1.Size = New System.Drawing.Size(40, 40)
         Me.ComboBox1.TabIndex = 5
         '
-        'PictureBox3
+        'Panel2
         '
-        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
-        Me.PictureBox3.Location = New System.Drawing.Point(63, 26)
-        Me.PictureBox3.Name = "PictureBox3"
-        Me.PictureBox3.Size = New System.Drawing.Size(40, 40)
-        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox3.TabIndex = 13
-        Me.PictureBox3.TabStop = False
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.PictureBox5)
+        Me.Panel2.Controls.Add(Me.PictureBox3)
+        Me.Panel2.Controls.Add(Me.TextBoxContrasenya)
+        Me.Panel2.Controls.Add(Me.LabelUsuari)
+        Me.Panel2.Controls.Add(Me.ButtonLogin)
+        Me.Panel2.Controls.Add(Me.Label1)
+        Me.Panel2.Controls.Add(Me.CheckBoxRecordar)
+        Me.Panel2.Controls.Add(Me.TextBoxUsuari)
+        Me.Panel2.Location = New System.Drawing.Point(49, 75)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(400, 290)
+        Me.Panel2.TabIndex = 12
         '
         'PictureBox5
         '
         Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
-        Me.PictureBox5.Location = New System.Drawing.Point(64, 109)
+        Me.PictureBox5.Location = New System.Drawing.Point(7, 93)
         Me.PictureBox5.Name = "PictureBox5"
         Me.PictureBox5.Size = New System.Drawing.Size(40, 40)
         Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.PictureBox5.TabIndex = 14
         Me.PictureBox5.TabStop = False
         '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(6, 10)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(40, 40)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 13
+        Me.PictureBox3.TabStop = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoValidate = System.Windows.Forms.AutoValidate.Disable
-        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(39, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(500, 400)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(500, 396)
         Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.Panel1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
@@ -240,8 +240,8 @@ Partial Class Form1
         Me.Panel1.PerformLayout()
         Me.Panel2.ResumeLayout(False)
         Me.Panel2.PerformLayout()
-        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
