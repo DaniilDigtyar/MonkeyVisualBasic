f55ad057d0e8c919aed58ed73567594c208d70dd
Daniil Digtyar <daniildigtyar@gmail.com>
2018-04-17 17:15:44 +0200

    Added vertical menu funcionaliti

---
+++ AM3D/MenuPrincipal.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MenuPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MenuPrincipal))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PanelControl = New System.Windows.Forms.Panel()
+        Me.LabelControl = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.LabelLogo = New System.Windows.Forms.Label()
+        Me.MenuVertical = New System.Windows.Forms.Panel()
+        Me.PictureBoxArrows = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.PanelControl.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuVertical.SuspendLayout()
+        CType(Me.PictureBoxArrows, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(78, Byte), Integer), CType(CType(114, Byte), Integer), CType(CType(127, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Panel4)
+        Me.Panel1.Controls.Add(Me.PanelControl)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.PictureBox4)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Controls.Add(Me.LabelLogo)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(788, 50)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel4
+        '
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Controls.Add(Me.Label2)
+        Me.Panel4.Location = New System.Drawing.Point(300, 0)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(100, 50)
+        Me.Panel4.TabIndex = 8
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(10, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(74, 25)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Gestió"
+        '
+        'PanelControl
+        '
+        Me.PanelControl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PanelControl.Controls.Add(Me.LabelControl)
+        Me.PanelControl.Location = New System.Drawing.Point(193, 0)
+        Me.PanelControl.Name = "PanelControl"
+        Me.PanelControl.Size = New System.Drawing.Size(100, 50)
+        Me.PanelControl.TabIndex = 7
+        '
+        'LabelControl
+        '
+        Me.LabelControl.AutoSize = True
+        Me.LabelControl.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelControl.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.LabelControl.Location = New System.Drawing.Point(10, 15)
+        Me.LabelControl.Name = "LabelControl"
+        Me.LabelControl.Size = New System.Drawing.Size(81, 25)
+        Me.LabelControl.TabIndex = 0
+        Me.LabelControl.Text = "Control"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(736, 6)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(40, 40)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 5
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(690, 6)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(40, 40)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 6
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(32, 40)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'LabelLogo
+        '
+        Me.LabelLogo.AutoSize = True
+        Me.LabelLogo.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelLogo.ForeColor = System.Drawing.Color.FromArgb(CType(CType(157, Byte), Integer), CType(CType(228, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.LabelLogo.Location = New System.Drawing.Point(44, 21)
+        Me.LabelLogo.Name = "LabelLogo"
+        Me.LabelLogo.Size = New System.Drawing.Size(143, 25)
+        Me.LabelLogo.TabIndex = 3
+        Me.LabelLogo.Text = "AMD3D - Inici"
+        '
+        'MenuVertical
+        '
+        Me.MenuVertical.BackColor = System.Drawing.Color.FromArgb(CType(CType(47, Byte), Integer), CType(CType(68, Byte), Integer), CType(CType(76, Byte), Integer))
+        Me.MenuVertical.Controls.Add(Me.PictureBoxArrows)
+        Me.MenuVertical.Location = New System.Drawing.Point(0, 50)
+        Me.MenuVertical.Name = "MenuVertical"
+        Me.MenuVertical.Size = New System.Drawing.Size(70, 450)
+        Me.MenuVertical.TabIndex = 1
+        Me.MenuVertical.Visible = False
+        '
+        'PictureBoxArrows
+        '
+        Me.PictureBoxArrows.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBoxArrows.Image = CType(resources.GetObject("PictureBoxArrows.Image"), System.Drawing.Image)
+        Me.PictureBoxArrows.Location = New System.Drawing.Point(7, 390)
+        Me.PictureBoxArrows.Name = "PictureBoxArrows"
+        Me.PictureBoxArrows.Size = New System.Drawing.Size(53, 50)
+        Me.PictureBoxArrows.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxArrows.TabIndex = 0
+        Me.PictureBoxArrows.TabStop = False
+        '
+        'MenuPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(788, 500)
+        Me.Controls.Add(Me.MenuVertical)
+        Me.Controls.Add(Me.Panel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "MenuPrincipal"
+        Me.Text = "Menu"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.PanelControl.ResumeLayout(False)
+        Me.PanelControl.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuVertical.ResumeLayout(False)
+        CType(Me.PictureBoxArrows, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents LabelLogo As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents MenuVertical As Panel
+    Friend WithEvents PanelControl As Panel
+    Friend WithEvents LabelControl As Label
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBoxArrows As PictureBox
+End Class
---
+++ AM3D/MenuPrincipal.vb
@@ -0,0 +1,10 @@
+﻿Public Class MenuPrincipal
+    Private Sub PictureBoxArrows_Click(sender As Object, e As EventArgs) Handles PictureBoxArrows.Click
+        MenuVertical.Width = 140
+        'PictureBoxArrows.Image = AM3D.Resources.Img.arrows_left
+    End Sub
+
+    Private Sub LabelControl_Click(sender As Object, e As EventArgs) Handles LabelControl.Click
+        MenuVertical.Show()
+    End Sub
+End Class
