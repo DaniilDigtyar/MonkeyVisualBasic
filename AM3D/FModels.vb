6e9e407adc4c661a18e5139d34baa5aa6040b8cf
Tiberiu Tasnadi <ttasnadi.olivera@gmail.com>
2018-05-01 20:09:24 +0200

    click menu marca

--- AM3D/FModels.vb
+++ AM3D/FModels.vb
@@ -1,3 +1,17 @@
 ﻿Public Class FModels
+    Private Sub BTModelAfegir_Click(sender As Object, e As EventArgs) Handles BTModelAfegir.Click
 
+    End Sub
+
+    Private Sub BTModelModificar_Click(sender As Object, e As EventArgs) Handles BTModelModificar.Click
+
+    End Sub
+
+    Private Sub BTImpressionsCancelar_Click(sender As Object, e As EventArgs) Handles BTImpressionsCancelar.Click
+
+    End Sub
+
+    Private Sub BTMarcaModificar_Click(sender As Object, e As EventArgs) Handles BTMarcaModificar.Click
+
+    End Sub
 End Class
